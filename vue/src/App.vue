cef4a6f1640158220271734ff2ee3a6a394d6219
RomanTsukanov <RomanTsukanov@users.noreply.github.com>
2021-03-09 18:42:15 +0300

    Update App.vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -5,16 +5,15 @@
         :grouped="true"
         :search-enabled="true"
         search-expr="Name"
-        :allow-item-deleting="true"
-    >
-        <DxItemDragging :allow-reordering="true"
-        >
-        </DxItemDragging>
+        :allow-item-deleting="true">
+        <DxItemDragging
+            :allow-reordering="true"
+        />
     </DxList>
 </template>
 
 <script>
-import DxList, {DxItemDragging} from "devextreme-vue/list";
+import DxList, { DxItemDragging } from "devextreme-vue/list";
 import DataSource from "devextreme/data/data_source";
 import { products } from './products'
 
